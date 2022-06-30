6442b8a43848e4db0b9564bd05eab861a341aae9
Gautam Kumar <54227183+gautamkr1876@users.noreply.github.com>
2022-06-30 21:19:50 +0530

    Update App.js

--- Problem-01/src/App.js
+++ Problem-01/src/App.js
@@ -1,10 +1,34 @@
 import "./styles.css";
-
+import objectData from "./objectData";
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div
+      style={{ margin: "auto", width: "70%", paddingTop: 40 }}
+      className="App"
+    >
+      <DisplayObject object={objectData}></DisplayObject>
     </div>
   );
 }
+
+const DisplayObject = ({ object }) => {
+  console.log(Object.entries(object));
+  return (
+    <>
+      {Object.entries(object).map(([key, value]) => {
+        if (typeof value === "object") {
+          return (
+            <>
+              <p>{`${key}: `}</p>
+              <div style={{ paddingLeft: 50 }}>
+                <DisplayObject object={value}></DisplayObject>
+              </div>
+            </>
+          );
+        } else {
+          return <p>{`${key}: ${value}`}</p>;
+        }
+      })}
+    </>
+  );
+};
